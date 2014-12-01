b919e32ee22c3f253dae1924412afff339fada5c
Mario Aichinger <aichingm@gmail.com>
2014-12-01 22:59:37 +0100

    fixed endless asking in Integer

--- examples/out.php
+++ examples/out.php
@@ -1,5 +1,2 @@
 <?php
-$config['database_type'] = 'sqlite';
-$config['sqlite_file'] = 'out.php';
-$config['database_table_prefix'] = 'hp_';
-$config['database_database'] = 'main';
+$config['linkIo_version'] = 1;
