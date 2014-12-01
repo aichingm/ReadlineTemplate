2ae13aa6e3c7b6c3b6defc492f9189b66d181f63
Mario Aichinger <aichingm@gmail.com>
2014-12-01 20:07:52 +0100

    fixes in the Number reader

--- src/phpConfigMaker/reader/Number.php
+++ src/phpConfigMaker/reader/Number.php
@@ -1,8 +1,18 @@
 <?php
+
 namespace phpConfigMaker\reader;
+
 use phpConfigMaker\SettingReader;
+
+/**
+ * This {@see SettingReader} can handle "Number" elements
+ * @author Mario Aichinger <aichingm@gmail.com>
+ */
 class Number extends SettingReader {
 
+    /**
+     * {@inheritdoc}
+     */
     public function ask($prompt) {
         do {
             $line = $this->readlineTrimAll($prompt);
@@ -10,13 +20,19 @@ class Number extends SettingReader {
         return doubleval($line);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function convertDefault($default) {
         return doubleval($default);
     }
 
+    /**
+     * Returns the type of element which can be handled by this class in this case "Number"
+     * @return string Returns "Number"
+     */
     public function handleElement() {
-        return "Text";
+        return "Number";
     }
 
 }
-
