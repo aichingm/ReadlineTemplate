ea6254b40b7b74f96342ba895a7b6b723425f6a3
Mario Aichigner <aichingm@gmail.com>
2016-07-27 11:45:53 +0200

    fix locale format in the regex example

--- examples/reading-a-regex.php
+++ examples/reading-a-regex.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../src/Autoload.php';
 
 $template = <<<EOF
 <Template>
-    <Regex key="fancyString" prompt="What is your locale? " pattern="[A-Z]{2}_[a-z]{2}"/>
-    <Regex key="superFancyString" prompt="Come on what is your locale? " pattern="~^[A-Z]{2}_[a-z]{2}$~" raw="true"/>
+    <Regex key="fancyString" prompt="What is your locale? " pattern="[a-z]{2}_[A-Z]{2}"/>
+    <Regex key="superFancyString" prompt="Come on what is your locale? " pattern="~^[a-z]{2}_[A-Z]{2}$~" raw="true"/>
 </Template> 
 EOF;
 
